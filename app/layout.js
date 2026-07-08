904051d95227b66a18d8beb22c0613835768763d
basquetballafcurico <basquetball.afcurico@gmail.com>
2026-07-08 13:33:26 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Tesorería Club Alianza Francés',
+  title: 'Tesorería Club Alianza Francesa',
   description: 'Control de ingresos y egresos del club',
 };
 
